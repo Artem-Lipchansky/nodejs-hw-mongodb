0d6541578659cb3158c0cad80973394f9fa957a3
Artem Lipchansky <lipchanskiy.an@gmail.com>
2024-06-29 22:24:14 +0300

    add

--- src/db/initMongoDB.js
+++ src/db/initMongoDB.js
@@ -1,23 +1,4 @@
-// import mongoose from 'mongoose';
-// import { env } from '../utils/env.js';
 
-// export const initMongoDB = async () => {
-//   try {
-//     const user = env('MONGODB_USER');
-//     const pwd = env('MONGODB_PASSWORD');
-//     const url = env('MONGODB_URL');
-//     const db = env('MONGODB_DB');
-
-//     const connectionString = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=nodejs-hw-mongodb`;
-
-//     await mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-
-//     console.log('Mongo connection successfully established!');
-//   } catch (e) {
-//     console.log('Error while setting up mongo connection', e);
-//     throw e;
-//   }
-// };
 
 import mongoose from 'mongoose';
 import { env } from '../utils/env.js';
