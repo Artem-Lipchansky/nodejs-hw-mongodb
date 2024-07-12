7f6263ce380aa80e6bcd3932b425b374084c2545
Artem Lipchansky <lipchanskiy.an@gmail.com>
2024-07-12 17:51:04 +0300

    add

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import contactsRouter from './routers/contacts.js';
+import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-
+import cookieParser from 'cookie-parser';
 
 
 import { env } from './utils/env.js';
@@ -34,9 +34,13 @@ export const setupServer = () => {
     });
   });
 
- app.use(contactsRouter);
+  app.use(cookieParser());
+
+  app.use(router);
 
   app.use('*', notFoundHandler);
+
+
 
   app.use(errorHandler);
 
